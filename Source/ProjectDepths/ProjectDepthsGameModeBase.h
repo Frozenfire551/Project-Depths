74eab481d4d84f392d5a58a6a57b088c9eb76a85
Frozenflame <nikgreen496@gmail.com>
2024-01-22 10:29:04 +1300

    Unreral 5.2 Init

---
+++ Source/ProjectDepths/ProjectDepthsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectDepthsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDEPTHS_API AProjectDepthsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
