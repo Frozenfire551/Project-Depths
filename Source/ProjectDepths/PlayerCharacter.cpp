4b7087493f58a0b8fc52eca38bc9fdbe1de02579
Frozenflame <nikgreen496@gmail.com>
2024-01-25 13:16:59 +1300

    base controller add. still need button logic

--- Source/ProjectDepths/PlayerCharacter.cpp
+++ Source/ProjectDepths/PlayerCharacter.cpp
@@ -2,9 +2,13 @@
 
 
 #include "PlayerCharacter.h"
+#include "Engine/GameEngine.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
@@ -21,6 +25,7 @@ APlayerCharacter::APlayerCharacter()
 	Carema = CreateDefaultSubobject<UCameraComponent>(TEXT("Carema"));
 	Carema->SetupAttachment(SpringArm);
 
+	
 }
 
 // Called when the game starts or when spawned
@@ -28,6 +33,17 @@ void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerCont = Cast<APlayerController>(GetController()))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("sec 1"));
+		if (UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerCont->GetLocalPlayer()))
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("sec 2"));
+			subsystem->AddMappingContext(InputMapping, 0);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("input added"));
+		}
+
+	}
 }
 
 // Called every frame
@@ -42,5 +58,46 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (TObjectPtr<UEnhancedInputComponent> enchancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		enchancedInputComp->BindAction(IA_Move, ETriggerEvent::Triggered, this, &APlayerCharacter::move);
+		enchancedInputComp->BindAction(IA_CameraMove, ETriggerEvent::Triggered, this, &APlayerCharacter::look);
+		enchancedInputComp->BindAction(IA_Jump, ETriggerEvent::Triggered, this, &APlayerCharacter::jump);
+		enchancedInputComp->BindAction(IA_Crouch, ETriggerEvent::Triggered, this, &APlayerCharacter::couch);
+		enchancedInputComp->BindAction(IA_Interact, ETriggerEvent::Triggered, this, &APlayerCharacter::intact);
+		enchancedInputComp->BindAction(IA_EquipmentWheel, ETriggerEvent::Triggered, this, &APlayerCharacter::changeItem);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("Controller Failed"));
+	}
 }
 
+void APlayerCharacter::move(const FInputActionValue& _Value)
+{
+}
+
+void APlayerCharacter::look(const FInputActionValue& _Value)
+{
+}
+
+void APlayerCharacter::jump()
+{
+	Jump();
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player Press A"));
+}
+
+void APlayerCharacter::couch()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player Press B"));
+}
+
+void APlayerCharacter::intact()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player Press X"));
+}
+
+void APlayerCharacter::changeItem()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player Press Y"));
+}
--- Source/ProjectDepths/PlayerCharacter.h
+++ Source/ProjectDepths/PlayerCharacter.h
@@ -4,13 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputAction.h"
 #include "PlayerCharacter.generated.h"
 
 
 class USpringArmComponent;
 class UCameraComponent;
 class USkeletalMeshComponent;
-
+class UInputMappingContext;
 UCLASS()
 
 class PROJECTDEPTHS_API APlayerCharacter : public ACharacter
@@ -28,6 +29,22 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* SkeletalMesh;
 
+	UPROPERTY(EditAnywhere, Category = "Input")
+		TObjectPtr<UInputMappingContext> InputMapping;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		TObjectPtr<UInputAction> IA_Move;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		TObjectPtr<UInputAction> IA_CameraMove;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		TObjectPtr<UInputAction> IA_Jump;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		TObjectPtr<UInputAction> IA_Crouch;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		TObjectPtr<UInputAction> IA_Interact;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+		TObjectPtr<UInputAction> IA_EquipmentWheel;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -38,5 +55,11 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
+	void move(const FInputActionValue& _Value);
+	void look(const FInputActionValue& _Value);
+	void jump();
+	void couch();
+	void intact();
+	void changeItem();
 };
